3873a957715340ddffe6bf90c5656cac3030f5c0
lalomax <lalomax365@gmail.com>
2022-07-06 20:47:12 -0400

    Aumentado la suma de 3 números

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Coche miCoche = new Coche();
 
+        System.out.println("La suma es :" + suma(10,20,30));
+
+        Coche miCoche = new Coche();
         miCoche.AumentarPuerta();
         System.out.println("El número de puertas es : " + miCoche.puertas);
+    }
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
     }
 }
 
